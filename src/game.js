9a59ef101f314c20e081c6dac1f20363cad734ad
bbennett <tbbennett1@gmail.com>
2019-10-17 17:03:05 -0700

    trying to pass through obstacles into game canvas

--- src/game.js
+++ src/game.js
@@ -12,7 +12,7 @@ class Game {
     this.boarder = new Boarder(this.cw, this.ch, ctx);
     this.gameObstacles = new Obstacles(this.cw, this.ch, ctx);
     document.addEventListener('keydown', this.handleEvent.bind(this));
-
+    
     this.draw();
   }
   draw() {
@@ -22,19 +22,19 @@ class Game {
     ctx.clearRect(0, 0, cw, ch);
     this.score++;
 
-    const title = ctx.createLinearGradient(0, 0, 0, 170);
-    title.addColorStop(0.25, "black");
+    const title = ctx.createLinearGradient(cw / 4, ch / 4, (2/4) * cw, (2/4) * ch);
+    title.addColorStop(0.6, "black");
     title.addColorStop(1, "green");
     ctx.fillStyle = title;
 
     if (this.score < 2) {
       ctx.textAlign = "center";
-      ctx.font = "30px Arial";
-      ctx.fillText(`BackCountry`, cw / 2, 60);
+      ctx.font = "80px Arial";
+      ctx.fillText(`BackCountry`, cw / 2, ch / 2);
       ctx.font = "20px Arial";
-      ctx.fillText(`Press the Space Bar to Start`, cw / 2, 100);
+      ctx.fillText(`Press the Space Bar to Start`, cw / 2, ((ch/2) + 30));
       ctx.font = "16px Arial";
-      ctx.fillText(`use arrow keys to steer`, cw / 2, 124);
+      ctx.fillText(`use arrow keys to steer`, cw / 2, ((ch/2) + 55));
     }
     // Score
     ctx.textAlign = "start";
@@ -43,10 +43,11 @@ class Game {
     ctx.fillText(`Score: ${Math.floor((this.score - 1) / 5)} feet`, 40, 50);
 
     if(this.obsPosY < ch){
-      this.obsPosY = this.obsPosY - 5;
+      this.obsPosY = this.obsPosY - 2;
     }
     this.boarder.draw();
-    this.gameObstacles.draw(this.obsPosY);
+    this.gameObstacles.draw();
+    debugger
   }
 
   handleEvent(e) {
@@ -60,7 +61,8 @@ class Game {
   }
 
   start() {
-    setInterval(this.draw.bind(this), 20);
+    setInterval(this.gameObstacles.draw.call(this, this.obsPosY), 200);
+    // setInterval(this.draw.bind(this), 20);
   }
 
 // stop() {
--- src/obstacles.js
+++ src/obstacles.js
@@ -3,28 +3,31 @@ class Obstacles {
     this.cw = cw;
     this.ch = ch;
     this.ctx = ctx;
-    this.obsImage = new Image();
-    this.obsImage.src = "../Obstacles.png";
+    this.obstacles = [];
   }
 
-  draw(obsPosY) {
-    // debugger
+  draw() { //obsPosY
+    let obsImage = new Image();
+    obsImage.src = "../Obstacles.png";
     let frameIndex = Math.floor(Math.random() * 4) * 50;
     let obsPosX = Math.floor(Math.random() * this.cw);
-    // let obsPosY = Math.floor(Math.random() * this.ch);
+    let obsPosY = Math.floor(Math.random() * this.ch);
 
-    this.ctx.drawImage(
-      this.obsImage, 
-      frameIndex, 
-      0, 
-      50, 
-      50, 
-      obsPosX, 
-      obsPosY, 
-      50, 
-      50);
+    obsImage.onload = function() {
+      this.ctx.drawImage(
+        obsImage, 
+        frameIndex, 
+        0, 
+        50, 
+        50, 
+        obsPosX, 
+        obsPosY, 
+        50, 
+        50);
+    }
+
+    return obsImage;
   }
-
 }
 
 export default Obstacles;
